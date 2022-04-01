6a4a54c6abe29ca4b0e31afa0c210dab2bf4f911
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-01 21:58:26 +0300

    Used binding feature

--- AngularCourse/Basic/src/app/post/post.component.ts
+++ AngularCourse/Basic/src/app/post/post.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
 
-  constructor() { }
+export class PostComponent {
+  img = 'https://s3.amazonaws.com/ckl-website-static/wp-content/uploads/2016/08/Banner_reactnative4-300x300.jpg'
 
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(
+      () => {
+        console.log('We changed image')
+        this.img = 'https://spng.subpng.com/20180329/jue/kisspng-angularjs-dart-front-and-back-ends-npm-escalator-5abda7d6ba62f5.4348649815223787107634.jpg'
+      }, 5000
+    )
   }
-
 }
