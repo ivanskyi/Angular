1ec2868f43b423cf21c3ce2797938cb57e5a1b29
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-02 19:59:35 +0300

    I used @Input for iterate posts

--- AngularCourse/ComponentsDetail/src/app/app.component.ts
+++ AngularCourse/ComponentsDetail/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ComponentsDetail';
+  posts: Post[] = [
+    { title: 'I want', text: 'I want learn Angular', id: 1 },
+    { title: 'I have', text: 'I have a dog', id: 2 },
+    { title: 'I am', text: 'I am Ukrainian', id: 3 }
+  ]
 }
--- AngularCourse/ComponentsDetail/src/app/post/post.component.ts
+++ AngularCourse/ComponentsDetail/src/app/post/post.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
+
 export class PostComponent implements OnInit {
+
+  @Input('myPost') post?: Post
 
   constructor() { }
 
