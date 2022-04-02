5690cb1545700cd81f39483b25afec26bf2490c5
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-02 11:05:08 +0300

    Added first event binding

--- AngularCourse/Basic/src/app/post/post.component.ts
+++ AngularCourse/Basic/src/app/post/post.component.ts
@@ -7,14 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PostComponent {
-  img = 'https://s3.amazonaws.com/ckl-website-static/wp-content/uploads/2016/08/Banner_reactnative4-300x300.jpg'
 
+  inputValue = ''
   constructor() {
-    setTimeout(
-      () => {
-        console.log('We changed image')
-        this.img = 'https://spng.subpng.com/20180329/jue/kisspng-angularjs-dart-front-and-back-ends-npm-escalator-5abda7d6ba62f5.4348649815223787107634.jpg'
-      }, 5000
-    )
+  }
+
+  onInput(event: any) {
+    console.log('Event', event)
+    this.inputValue = event.target.value
   }
 }
