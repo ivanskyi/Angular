60b19f584493e47ede1db2e862bb9ee8465a3beb
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-03 12:20:57 +0300

    Tested lifecycle hooks and added opportunity to remove post

--- AngularCourse/ComponentsDetail/src/app/app.component.ts
+++ AngularCourse/ComponentsDetail/src/app/app.component.ts
@@ -22,4 +22,8 @@ export class AppComponent {
     this.posts.unshift(post)
 
   }
+
+  removePost(id: number) {
+    this.posts = this.posts.filter(p => p.id !== id)
+  }
 }
--- AngularCourse/ComponentsDetail/src/app/post/post.component.ts
+++ AngularCourse/ComponentsDetail/src/app/post/post.component.ts
@@ -1,4 +1,13 @@
+import { OnChanges, SimpleChanges } from '@angular/core';
+import { AfterContentInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { Output } from '@angular/core';
+import { AfterViewChecked } from '@angular/core';
+import { AfterContentChecked } from '@angular/core';
+import { DoCheck } from '@angular/core';
 import { Component, OnInit, Input, ContentChild, ElementRef } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -7,15 +16,58 @@ import { Post } from '../app.component';
   styleUrls: ['./post.component.scss']
 })
 
-export class PostComponent implements OnInit {
+export class PostComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   @Input('myPost') post?: Post
   @ContentChild('info', { static: true }) infoRef?: ElementRef
+  @Output() onRemove = new EventEmitter<number>()
 
-  constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.infoRef?.nativeElement)
+  constructor() {
+    console.log('constructor')
   }
 
-}
+  ngOnInit(): void {
+    console.log('ngOnInit()')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges')
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked')
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy')
+  }
+
+  removePost() {
+    this.onRemove.emit(this.post?.id)
+  }
+
+} 
