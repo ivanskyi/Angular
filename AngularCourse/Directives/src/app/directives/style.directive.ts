88c057523be4125142600db361af9be72623f4a4
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-04 23:30:08 +0300

    I used @HostListener

--- AngularCourse/Directives/src/app/directives/style.directive.ts
+++ AngularCourse/Directives/src/app/directives/style.directive.ts
@@ -1,4 +1,5 @@
 import { Renderer2 } from "@angular/core";
+import { HostListener } from "@angular/core";
 import { ElementRef } from "@angular/core";
 import { Directive } from "@angular/core";
 
@@ -7,6 +8,18 @@ import { Directive } from "@angular/core";
 })
 export class StyleDirective {
     constructor(private el: ElementRef, private r: Renderer2) {
-        this.r.setStyle(el.nativeElement, 'color', 'green')
+        this.r.setStyle(el.nativeElement, 'color', 'red')
+    }
+
+    @HostListener('click', ['$event.target']) onClick(event: Event) {
+        console.log(event)
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', 'green')
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null)
     }
 }
