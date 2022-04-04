a632b6f7d52b7940cc9ee3f7783da9c160595bcd
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-04 22:43:46 +0300

    Added implementation for style directive

--- AngularCourse/Directives/src/app/directives/style.directive.ts
+++ AngularCourse/Directives/src/app/directives/style.directive.ts
@@ -1,8 +1,12 @@
+import { Renderer2 } from "@angular/core";
+import { ElementRef } from "@angular/core";
 import { Directive } from "@angular/core";
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
-
+    constructor(private el: ElementRef, private r: Renderer2) {
+        this.r.setStyle(el.nativeElement, 'color', 'green')
+    }
 }
