cef47990f461ba0a2f0dcf5ccdab0adf9a04c9cc
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-05 21:49:46 +0300

    Added structural directive

---
+++ AngularCourse/Directives/src/app/directives/ifnot.directive.ts
@@ -0,0 +1,23 @@
+import { ViewContainerRef } from '@angular/core';
+import { Input } from '@angular/core';
+import { TemplateRef } from '@angular/core';
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appIfnot]'
+})
+export class IfnotDirective {
+  @Input('appIfnot') set ifNot(condition: boolean) {
+    if (!condition) {
+      this.viewContainer.createEmbeddedView(this.templateRef)
+    } else {
+      this.viewContainer.clear()
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef) {
+
+  }
+
+}
