9e0b767a3f8c597fcfdfed9609f55eaecd72107e
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-05 21:25:10 +0300

    I used @HostBinding

--- AngularCourse/Directives/src/app/directives/style.directive.ts
+++ AngularCourse/Directives/src/app/directives/style.directive.ts
@@ -1,5 +1,6 @@
 import { Renderer2 } from "@angular/core";
 import { Input } from "@angular/core";
+import { HostBinding } from "@angular/core";
 import { HostListener } from "@angular/core";
 import { ElementRef } from "@angular/core";
 import { Directive } from "@angular/core";
@@ -10,6 +11,7 @@ import { Directive } from "@angular/core";
 export class StyleDirective {
     @Input('appStyle') color: string = 'blue'
     @Input() dStyles?: { border?: string, fontWeight?: string, borderRadius?: string }
+    @HostBinding('style.color') elColor: string | null = null
 
     constructor(private el: ElementRef, private r: Renderer2) {
         this.r.setStyle(el.nativeElement, 'color', 'red')
@@ -20,14 +22,14 @@ export class StyleDirective {
     }
 
     @HostListener('mouseenter') onEnter() {
-        this.r.setStyle(this.el.nativeElement, 'color', this.color)
+        this.elColor = this.color
         this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles?.fontWeight)
         this.r.setStyle(this.el.nativeElement, 'border', this.dStyles?.border)
         this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles?.borderRadius)
     }
 
     @HostListener('mouseleave') onLeave() {
-        this.r.setStyle(this.el.nativeElement, 'color', null)
+        this.elColor = null
         this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
         this.r.setStyle(this.el.nativeElement, 'border', null)
         this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
