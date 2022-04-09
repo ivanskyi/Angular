47cfa1501a282847952f1f172308acc218f36e81
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-09 19:50:36 +0300

    I created custom pipe

--- AngularCourse/Pipes/src/app/app.module.ts
+++ AngularCourse/Pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MultByPipe } from './pipes/mult-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultByPipe
   ],
   imports: [
     BrowserModule
