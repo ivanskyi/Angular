dbd1482989b9d89ac3348cc67707095ddc79f4d7
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-19 09:04:19 +0300

    I added filter by title

--- AngularCourse/Pipes/src/app/app.module.ts
+++ AngularCourse/Pipes/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MultByPipe } from './pipes/mult-by.pipe';
 import { MarksPipe } from './pipes/marks.pipe';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MultByPipe,
-    MarksPipe
+    MarksPipe,
+    FilterComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularCourse/Pipes/src/app/filter/filter.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Post {
+  title: string
+  text: string
+}
+
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent {
+
+  search = ''
+
+  posts: Post[] = [
+    { title: 'Beer', text: 'The best beer' },
+    { title: 'Bread', text: 'The best bread' }
+  ]
+}
