498fb1f1747f94da05b8e865ac5c2975cbfb35e8
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-21 20:28:50 +0300

    Added skipped files

--- AngularCourse/FormsAndValidation/src/app/my.validators.ts
+++ AngularCourse/FormsAndValidation/src/app/my.validators.ts
@@ -1,4 +1,5 @@
 import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
 
 export class MyValidators {
     static restrictedEmails(control: FormControl): { [key: string]: boolean } {
@@ -6,5 +7,17 @@ export class MyValidators {
             return { restrictedEmail: true }
         }
         return null as any
+    }
+
+    static uniqEmail(control: FormControl): Promise<any> | Observable<any> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                if (control.value === 'async@mail.com') {
+                    resolve({ uniqEmail: true })
+                } else {
+                    resolve(null as any)
+                }
+            }, 1000)
+        })
     }
 }
