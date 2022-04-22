ff5f4a4b90438f70aa549b948a5d2cb39be32cf6
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-04-22 20:57:07 +0300

    Added admin routings

--- AngularCourse/Blog/src/app/app-routing.module.ts
+++ AngularCourse/Blog/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PreloadAllModules } from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
@@ -11,11 +14,17 @@ const routes: Routes = [
       { path: '', component: HomePageComponent },
       { path: 'post/:id', component: PostPageComponent }
     ]
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(a => a.AdminModule)
   }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
